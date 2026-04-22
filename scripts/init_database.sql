df1e11c234fe01027cedadffacc3411cd08e059e
Sandeep Nayak <157904712+sandeepp089@users.noreply.github.com>
2026-04-22 14:43:18 +0530

    Refactor comments in init_database.sql
    Updated comments to improve formatting and clarity.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,7 +1,7 @@
 /*
-==========================================================
+===========================================================
 Create Database and Schemas
-==========================================================
+===========================================================
 Script Purpose:
   This script creates a new database named 'DataWarehouse' after checking if it already exists. 
   If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
